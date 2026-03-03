45beaf87573ce0e3caf855200b7c7827743dc373
Marienelle O. Pineda <marinelolazo00@gmail.com>
2026-03-03 11:45:05 +0800

    Add files via upload

---
+++ App Dev.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE birds_db;
+USE birds_db;
+
+CREATE TABLE birds (
+    bird_name VARCHAR(100)
+);
+INSERT INTO birds (bird_name) VALUES ('Hummingbird');
+
+CREATE DATABASE toys_db;
+USE toys_db;
+
+CREATE TABLE toys (
+    toy_name VARCHAR(100)
+);
+INSERT INTO toys (toy_name) VALUES ('Barbie');
+
+CREATE DATABASE movies_db;
+USE movies_db;
+
+CREATE TABLE movies (
+    movie_title VARCHAR(150)
+);
+INSERT INTO movies (movie_title) VALUES ('Stranger Things');
+
+CREATE DATABASE books_db;
+USE books_db;
+
+CREATE TABLE books (
+    book_title VARCHAR(150)
+);
+INSERT INTO books (book_title) VALUES ('Atomic Habits');
+
+CREATE DATABASE vehicle_db;
+USE vehicle_db;
+
+CREATE TABLE vehicle (
+    vehicle_brand VARCHAR(100)
+);
+INSERT INTO vehicle (vehicle_brand) VALUES ('Toyota');
+
